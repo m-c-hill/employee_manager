daa3c0cd036f647b00691d24f6cdf51fd66b9c51
m-c-hill <matthewhill2595@gmail.com>
2021-10-13 10:26:29 +0100

    final commit - assignment submitted

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 import json
 import os
 import sqlite3
+TESTING_MODE = True
 
 
 class DBOperations:
-    # Define DBOperation class to manage all data into the database.
+    """
+    Class to manage interactions with SQLite database.
+
+    sql_queries_dict : dict
+    conn : sqlite3.Connection
+    cur: sqlite3.Cursor
+    """
 
     def __init__(self):
 
-        # Import sql queries from json utils file
+        # Import SQL queries from json utils file
         with open('sql_queries.json') as sql_queries_file:
             self.sql_queries_dict = json.load(sql_queries_file)
 
@@ -23,10 +30,12 @@ class DBOperations:
             self.conn.close()
 
     def get_connection(self) -> None:
+        """Initialise connection with The Hive database"""
         self.conn = sqlite3.connect("TheHive.db")
         self.cur = self.conn.cursor()
 
     def create_table(self) -> None:
+        """Create the employee table"""
         try:
             self.get_connection()
             if not self.conn.execute(self.sql_queries_dict["check_table_exists"]).fetchone():
@@ -40,7 +49,8 @@ class DBOperations:
         finally:
             self.conn.close()
 
-    def insert_data(self):
+    def insert_data(self) -> None:
+        """Insert employee record"""
         try:
             self.get_connection()
 
@@ -60,12 +70,12 @@ class DBOperations:
         finally:
             self.conn.close()
 
-    def select_all(self):
+    def select_all(self) -> None:
+        """Select all records in the employee table and display them in a table"""
         try:
             self.get_connection()
             self.cur.execute(self.sql_queries_dict["sql_select_all"])
             results = self.cur.fetchall()
-
             Menu.display_results_table(results)
 
         except Exception as e:
@@ -73,7 +83,8 @@ class DBOperations:
         finally:
             self.conn.close()
 
-    def search_data(self):
+    def search_data(self) -> None:
+        """Search for record by employee ID or surname. Display results in table."""
         try:
             self.get_connection()
             search_type = input("\nSearch by id or by surname. Please enter 'i' or 's': ").strip().lower()
@@ -89,8 +100,7 @@ class DBOperations:
             result = self.cur.fetchall()
             if isinstance(result, list):
                 print(f"\nFound {len(result)} record matching your query:\n")
-                for index, detail in enumerate(result):
-                    Menu.display_results_table(result)
+                Menu.display_results_table(result)
             else:
                 print(f"No record(s) found.")
 
@@ -99,33 +109,31 @@ class DBOperations:
         finally:
             self.conn.close()
 
-    '''
-    def update_data(self):
+    def update_data(self) -> None:
+        """Update record by selecting an employee by ID"""
         try:
             self.get_connection()
 
-            # Update statement
-            valid_id = False
-            id_update = 0
-            while not valid_id:
-                try:
-                    id_update = input("\nSelect the ID of the employee record to update. To exit press q: ")
-                    if id_update.lower() == "q":
-                        return
-                    else:
-                        id_update = int(id_update)
-                    valid_id = True
-                except ValueError:
-                    print("Please try again - enter a valid integer ID.")
-
+            id_update = Validation.valid_id_input()
             result = self.cur.execute(self.sql_queries_dict["sql_select_id"], tuple(str(id_update))).fetchall()
 
             if len(result) != 0:
-                valid_field = False
-                field = ""
-                while not valid_field:
-                    input("Please input a field to update: ")
-                print(str(len(result)) + "Row(s) affected.")
+                Menu.display_results_table(result)
+                self.cur.execute(self.sql_queries_dict["sql_select_all"])
+                columns = [description[0] for description in self.cur.description]
+                column = Validation.valid_column_input(columns)
+                if column == "q":
+                    return
+
+                new_value = Validation.valid_update_value_input(column)
+
+                if column == "id" and self.check_id_exists(new_value):
+                    print("\nEmployee ID already exists. Returning to main menu.\n")
+                    return
+                print(new_value)
+                self.cur.execute(f"UPDATE employee SET { column } = ? WHERE id = ?", (new_value, id_update))
+                self.conn.commit()
+                print(str(len(result)) + "row(s) affected.")
             else:
                 print("Cannot find this record in the database")
 
@@ -133,11 +141,9 @@ class DBOperations:
             print(e)
         finally:
             self.conn.close()
-    '''
 
-    def delete_data(self):
-        # Define Delete_data method to delete data from the table. The user will need to input the employee id to
-        # delete the corresponding record.
+    def delete_data(self) -> None:
+        """Method to delete record from the table by selecting employee by ID"""
         try:
             self.get_connection()
             self.cur.execute(self.sql_queries_dict["sql_select_all"])
@@ -154,7 +160,7 @@ class DBOperations:
 
             if len(result) != 0:
                 print(str(len(result)) + " row(s) affected.")
-                result = self.cur.execute(self.sql_queries_dict["sql_delete_data"], (id_delete,)).fetchall()
+                self.cur.execute(self.sql_queries_dict["sql_delete_data"], (id_delete,)).fetchall()
                 self.conn.commit()
             else:
                 print("\nCannot find this record in the database.\n")
@@ -164,8 +170,74 @@ class DBOperations:
         finally:
             self.conn.close()
 
+    def check_id_exists(self, _id) -> bool:
+        """Method to check if an employee ID has already been taken"""
+        try:
+            self.get_connection()
+            self.cur.execute(self.sql_queries_dict["sql_select_id"], (_id,))
+            result = self.cur.fetchall()
+            return len(result) > 0
+        except Exception as e:
+            print(e)
+        finally:
+            self.conn.close()
+
+    def check_admin_username_exists(self, username) -> bool:
+        """Method to check if a given username exists in the admin table"""
+        try:
+            self.get_connection()
+            self.cur.execute(self.sql_queries_dict["check_admin_exists"], (username,))
+            result = self.cur.fetchall()
+            return len(result) > 0
+        except Exception as e:
+            print(e)
+        finally:
+            self.conn.close()
+
+    def create_admin_table(self) -> None:
+        """Method to create the admin table. This is for testing purposes and would be implemented correctly in
+        production"""
+        try:
+            self.get_connection()
+            self.cur.execute("DROP TABLE IF EXISTS admin")
+            self.cur.execute(self.sql_queries_dict["create_admin_table"])
+            self.conn.commit()
+        except Exception as e:
+            print(e)
+        finally:
+            self.conn.close()
+
+    def populate_admin_table(self) -> None:
+        """Method to populate the admin table. This is for testing purposes and would be implemented correctly in
+        production"""
+        try:
+            self.get_connection()
+            self.cur.execute(self.sql_queries_dict["populate_admin_table"])
+            self.conn.commit()
+        except Exception as e:
+            print(e)
+        finally:
+            self.conn.close()
+
+    def check_password(self, username, password) -> bool:
+        """Method to check if user's password matches their input password when logging in."""
+        try:
+            self.get_connection()
+            self.cur.execute(self.sql_queries_dict["get_admin_password"], (username,))
+            true_password = self.cur.fetchone()[0]
+            return true_password == password
+
+        except Exception as e:
+            print(e)
+        finally:
+            self.conn.close()
+
 
 class Employee:
+    """
+    Class to represent Employee with fields that reflect the columns in employee table in database
+    """
+
     def __init__(self):
         self.id = 0
         self.title = ""
@@ -174,81 +246,74 @@ class Employee:
         self.email = ""
         self.salary = 0.
 
-    def set_employee_id(self, id):
+    def set_employee_id(self, id) -> None:
         self.id = id
 
-    def set_employee_title(self, title):
+    def set_employee_title(self, title) -> None:
         self.title = title
 
-    def set_employee_forename(self, forename):
+    def set_employee_forename(self, forename) -> None:
         self.forename = forename
 
-    def set_employee_surname(self, surname):
+    def set_employee_surname(self, surname) -> None:
         self.surname = surname
 
-    def set_employee_email(self, email):
+    def set_employee_email(self, email) -> None:
         self.email = email
 
-    def set_employee_salary(self, salary):
+    def set_employee_salary(self, salary) -> None:
         self.salary = salary
 
-    def get_employee_id(self):
+    def get_employee_id(self) -> int:
         return self.id
 
-    def get_employee_title(self):
+    def get_employee_title(self) -> str:
         return self.title
 
-    def get_forename(self):
+    def get_forename(self) -> str:
         return self.forename
 
-    def get_surname(self):
+    def get_surname(self) -> str:
         return self.surname
 
-    def get_email(self):
+    def get_email(self) -> str:
         return self.email
 
-    def get_salary(self):
+    def get_salary(self) -> float:
         return self.salary
 
-    def employee_str_no_id(self):
+    def employee_str_no_id(self) -> str:
+        """String representation of employee, omitting user ID"""
         return self.title + "\n" + self.forename + "\n" + self.surname + "\n" + self.email + "\n" + str(
             self.salary)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return str(
             self.id) + "\n" + self.title + "\n" + self.forename + "\n" + self.surname + "\n" + self.email + "\n" + str(
             self.salary)
 
 
 class AdminLogin:
-
-    def __init__(self, id, email, username, password, date_of_birth):
-        self.email = email
-        self.username = username
-        self.password = password
-        self.date_of_birth = date_of_birth
-
-    def check_password(self, input_password):
-        pass
-
-    def reset_password(self):
-        date_of_birth = ""  # query the admin table
-        print(f"A password has been sent to your email address: {self.email}\n")
+    """
+    Admin class to assist with login functionality.
+    """
+    @staticmethod
+    def check_password(username, input_password) -> bool:
+        db_ops = DBOperations()
+        return db_ops.check_password(username, input_password)
 
     @staticmethod
-    def check_username_exists(username):
-        pass
+    def check_username_exists(username) -> bool:
+        db_ops = DBOperations()
+        return db_ops.check_admin_username_exists(username)
 
 
 class Menu:
-
+    """
+    Menu class to provide display functions for user interface.
+    """
     @staticmethod
     def display_logo() -> None:
-        """Prints the application logo, used in the main menu.
-
-        Returns:
-            None
-        """
         hive_logo = (
             "*=========================================*\n"
             "|  _____ _            _   _ _             |\n"
@@ -266,14 +331,9 @@ class Menu:
 
     @staticmethod
     def display_main_menu() -> None:
-        """Prints the main menu.
-
-        Returns:
-            None
-        """
         main_menu = (
             "Main Menu: \n\n"
-            " 1. Create table Employee table\n"
+            " 1. Create Employee table\n"
             " 2. Insert data into Employee table\n"
             " 3. Show all records in Employee table\n"
             " 4. Search for employee\n"
@@ -290,7 +350,7 @@ class Menu:
             "Welcome to The Hive - your one-stop solution for employee data management.\n\n"
             "This application allows you to store, update, delete and filter employee \n"
             "records. \n\n"
-            "Key fields stored in The Hive are:\n"
+            "Key columns stored in The Hive are:\n"
             "    - Employee ID\n"
             "    - Title\n"
             "    - Forename\n"
@@ -323,11 +383,35 @@ class Menu:
             print("No results to display.")
 
     @staticmethod
-    def login_menu():
-        pass
+    def login_menu() -> None:
+        """Method to take user's credentials and check them against the database"""
+        initialise_admin()
+        while True:
+            Menu.display_logo()
+            print("\nWelcome to The Hive. Please enter your login details below: \n")
+            if TESTING_MODE:
+                print("\nTESTING MODE ACTIVE FOR MARKER: To login, please enter the username 'admin' and the password "
+                      "'password123'\n")
+            print("\n===== Admin Login =====\n")
+
+            username_exists = False
+            username = ""
+            while not username_exists:
+                username = input("Admin username: ")
+                username_exists = AdminLogin.check_username_exists(username)
+                if not username_exists:
+                    print("Invalid username entered. Please try again. ")
+
+            input_password = input("Password: ")
+            if AdminLogin.check_password(username, input_password):
+                return
+            input("Incorrect username or password, please try again. Press enter to continue: ")
 
 
 class Validation:
+    """
+    Class to provide validation techniques for user input.
+    """
 
     @staticmethod
     def valid_title_input():
@@ -353,16 +437,66 @@ class Validation:
     def valid_id_input():
         while True:
             try:
-                id_delete = input("\nSelect an employee ID. To exit press q: ")
-                if id_delete.lower() == "q":
+                _id = input("\nSelect an employee ID. To exit press q: ")
+                if _id.lower() == "q":
                     return "q"
                 else:
-                    return int(id_delete)
+                    return int(_id)
             except ValueError:
                 print("Please try again - enter a valid integer ID.")
 
+    @staticmethod
+    def valid_column_input(columns):
+        while True:
+            for column_i in columns:
+                print(f"- {column_i}")
+            column_choice = input("\nPlease select a column from the above list. To quit, press 'q': ").strip().lower()
+            if column_choice in columns:
+                return column_choice
+            print("\nInvalid choice.\n")
 
-def run():
+    @staticmethod
+    def valid_update_value_input(column):
+        if column == "id":
+            while True:
+                try:
+                    id = input("\nEnter a new employee ID: ")
+                    return int(id)
+                except ValueError:
+                    print("Please try again - enter a valid integer ID.")
+
+        elif column == "title":
+            return Validation.valid_title_input()
+
+        elif column == "forename":
+            return input("\nEnter a new forename: ").lower().strip().capitalize()
+
+        elif column == "surname":
+            return input("\nEnter a new surname: ").lower().strip().capitalize()
+
+        elif column == "email":
+            return input("\nEnter a new email address: ").lower().strip()
+
+        elif column == "salary":
+            return Validation.valid_salary_input()
+
+        elif column == "q":
+            return "q"
+
+
+def initialise_admin() -> None:
+    """Method to initialise and populate the admin table, used for testing and demonstration purposes."""
+    db_ops = DBOperations()
+    db_ops.create_admin_table()
+    db_ops.populate_admin_table()
+
+
+def run() -> None:
+
+    # Login
+    Menu.login_menu()
+
+    # Once user credentials have been authenticated, continue to main menu
     while True:
         db_ops = DBOperations()
         Menu.display_logo()
